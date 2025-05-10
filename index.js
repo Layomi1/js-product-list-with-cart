e9a0e4eaf3d66d2085cce2cdb35fb7e00953ea0e
Layomi <mac@MACs-MacBook-Pro.local>
2025-05-10 15:20:32 +0100

    modal logic immplemented

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ fetch("./data.json")
     const menuList = menu
       .map(
         (item) =>
-          ` <figure class="max-w-[300px] h-auto gap-2 mb-5 relative">
+          ` <figure class="w-full max-w-[320px] h-auto gap-2 mb-5 relative">
               <img
                 src=${item.image.thumbnail}
                 alt=${item.name}
@@ -20,7 +20,7 @@ fetch("./data.json")
               </figcaption>
               <article class='switch'>
                 <button
-                    class="cart absolute flex justify-center items-center gap-2 top-[62%] left-[18%] text-sm bg-white border-[1px] border-[#c73a0f] py-4 px-6 rounded-full text-white"
+                    class="cart absolute flex justify-center items-center gap-2 top-[68%] left-[20%] text-sm bg-white border-[1px] border-[#c73a0f] py-4 px-6 rounded-full text-white"
                 >
                   <img src="./assets/images/icon-add-to-cart.svg" />
 
@@ -29,10 +29,10 @@ fetch("./data.json")
                    >
                 </button>
                 <article
-                    class="counter hidden absolute flex justify-center items-center gap-10 top-[62%] left-[10%] text-sm bg-[#c73a0f] py-4 px-6 rounded-full"
+                    class="counter hidden absolute flex justify-center items-center gap-10 top-[68%] left-[20%] text-sm bg-[#c73a0f] py-4 px-4 rounded-full"
                 >
                   <button
-                    class="reduceQuantity active:bg-white border-[1px] border-white rounded-full px-[7px] py-3"
+                    class="reduceQuantity active:bg-white border-[1px] border-white rounded-full grid place-items-center w-6 h-6"
                   >
                     <img
                       src="./assets/images/icon-decrement-quantity.svg"
@@ -42,11 +42,11 @@ fetch("./data.json")
 
                 <span class="quantity text-white" ></span>
                 <button
-                  class="addQuantity active:bg-white border-[1px] border-white rounded-full px-[7px] py-2"
+                  class="addQuantity active:bg-white border-[1px] border-white rounded-full h-6 w-6 grid place-items-center"
                   >
                   <img
                      src="./assets/images/icon-increment-quantity.svg"
-                       class="active:text-[#c73a0f]"
+                       class="active:fill-[#c73a0f]"
                   />
                 </button>
               </article>
@@ -70,7 +70,8 @@ fetch("./data.json")
     let cartDetails = document.querySelector("#cart-details");
     let emptyCart = document.querySelector(".empty-cart");
     const grandTotal = document.querySelector(".grand-total");
-    const grandTotalValue = document.querySelector("#grand-total-no");
+    const overlay = document.querySelector(".overlay");
+    const orderCard = document.querySelector(".order-card");
 
     /*******AddItem item from cart***************/
     addCartBtns.forEach((btn, index) => {
@@ -91,6 +92,10 @@ fetch("./data.json")
         cartDetails.innerHTML = "";
         return;
       }
+
+      const itemPrice = cart
+        .reduce((sum, item) => sum + item.quantity * item.price, 0)
+        .toFixed(2);
       emptyCart.classList.add("hidden");
       cartDetails.innerHTML = cart
         .map(
@@ -104,11 +109,9 @@ fetch("./data.json")
               <span class="cart-quantity text-[#c73a0f] text-[12px]" 
                 >${item.quantity}X</span
               >
-              <span class="text-[#c9aea6] text-[12px]">@ $${
-                item.price
-              }.00</span>
+              <span class="text-[#c9aea6] text-[12px]">@ $${item.price}.00</span>
               <span class="text-[87635a] text-[12px] font-medium" id="totalPrice"
-                >$${item.price * item.quantity}.00</span
+                >$${itemPrice}</span
               >
             </p>
           </article>
@@ -119,6 +122,7 @@ fetch("./data.json")
             <img src="/assets/images//icon-remove-item.svg" alt="cancel" />
           </button>
         </article>
+        
       `
         )
         .join("");
@@ -152,13 +156,35 @@ fetch("./data.json")
         return;
       }
       grandTotal.classList.remove("hidden");
-      const grandTotalValue = cart.reduce(
-        (sum, item) => sum + item.quantity * item.price,
-        0
-      );
+      const grandTotalValue = cart
+        .reduce((sum, item) => sum + item.quantity * item.price, 0)
+        .toFixed(2);
       grandTotal.innerHTML = ` 
-          <p>Order Total</p>
-          <span class="font-bold" id="grand-total-value">$${grandTotalValue}.00</span>`;
+
+         <article class="flex justify-between my-4">
+           <p>Order Total</p>
+          <span class="font-bold" id="grand-total-value">$${grandTotalValue}</span>
+         </article>
+        
+          
+        <figure
+          class="flex gap-2 items-center bg-[#f4edeb] p-2 justify-center rounded-[10px] mt-8 mb-4"
+        >
+          <img
+            src="/assets/images/icon-carbon-neutral.svg"
+            alt="carbon-neutral"
+          />
+          <figcaption class="text-[14px]">
+            This is a <span class="font-bold">carbon neutral </span> delivery
+          </figcaption>
+        </figure>
+        <article class="flex justify-center">
+          <button
+            class="confirm-btn text-white bg-[#c73a0f] py-2 px-20 text-nowrap text-md rounded-full active:bg-[#260f08]"
+          >
+            Confirm Order
+          </button>
+        </article>`;
     }
 
     /*******Increase item in Counter & cart ***************/
@@ -201,6 +227,79 @@ fetch("./data.json")
         }
         renderCartDetails();
       });
+    });
+
+    /**********************Confirm Order***************************/
+
+    grandTotal.addEventListener("click", (e) => {
+      const confirmOrderBtn = e.target.closest(".confirm-btn");
+      if (!confirmOrderBtn) return;
+      overlay.classList.remove("hidden");
+
+      orderCard.innerHTML = `${cart
+        .map(
+          (item) =>
+            `<article class="flex bg-[#f4edeb]  p-4 justify-between items-center border-b-[1px] border-[c9aea6]">
+          
+              <figure class="flex gap-4 items-center">
+                <img
+                  src=${item.image.thumbnail}
+                  alt="cart-item image"
+                  class="w-full max-w-[100px] md:max-w-50 h-auto object-cover rounded-md"
+                />
+                <figcaption>
+                  <p class="text-[#260f08] font-medium truncate...">${
+                    item.name
+                  }</p>
+                  <p>
+                    <span class="cart-quantity text-[#c73a0f] text-[12px]">
+                      ${item.quantity}X
+                    </span>
+                    <span class="text-[#c9aea6] text-[12px]">
+                      @ $${item.price}.00
+                    </span>
+                  </p>
+                </figcaption>
+              </figure>
+              <span class="text-[87635a] text-[12px] font-medium" id="totalPrice">
+                $${item.price * item.quantity}.00
+              </span>
+            </article>`
+        )
+        .join("")}
+
+      
+          <article class="flex bg-[#f4edeb] justify-between py-6 px-4">
+                <p>Order Total</p>
+              <span class="font-bold" >$${cart
+                .reduce((sum, item) => sum + item.quantity * item.price, 0)
+                .toFixed(2)}</span>
+              </article>
+      
+            <article class="flex justify-center mt-4">
+              <button
+                class="start-order-btn text-white bg-[#c73a0f] py-2 px-20 md:px-40 text-nowrap text-md rounded-full active:bg-[#260f08]"
+              >
+                Start New Order
+              </button>
+            </article>
+           `;
+    });
+    /**********************Confirm Order  Ends***************************/
+
+    orderCard.addEventListener("click", (e) => {
+      const resetBtn = e.target.closest(".start-order-btn");
+      overlay.classList.add("hidden");
+
+      emptyCart.classList.remove("hidden");
+      cart = [];
+      cartDetails.innerHTML = "";
+      grandTotal.innerHTML = "";
+      orderCard.innerHTML = "";
+      quantityDisplays.forEach((display) => (display.textContent = 0));
+      counters.forEach((counter) => counter.classList.add("hidden"));
+
+      addCartBtns.forEach((btn) => btn.classList.remove("hidden"));
     });
   })
   .catch((err) => {
